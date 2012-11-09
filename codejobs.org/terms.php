fc561be8a01573e4b94a80b0600c1bc29cb6c444
psy <psy@ventiska.(none)>
2012-11-09 04:45:01 +0100

    added creative commons by-sa 2.0 license to terms of service

--- codejobs.org/terms.php
+++ codejobs.org/terms.php
@@ -12,28 +12,33 @@
         <h3>Terms</h3>
         <p>By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable<br /> 
            laws and regulations, and agree that you are responsible for compliance with any applicable local laws.<br /><br /> 
-           If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained<br />
-           in this web site are protected by applicable copyright and trade mark law.</p>
+           If you do not agree with any of these terms, you are prohibited from using or accessing this site. All materials contained<br />
+           in this web site are under license: <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC BY-SA 2.0</a></p>
         <h3>Use License</h3>
 
       <ol type="a">
 	<li>
-		Permission is granted to temporarily download one copy of the materials (information or software)<br /> 
-                on CodeJobs.org's web site for personal, non-commercial transitory viewing only. This is the grant of a license,<br />
-		not a transfer of title, and under this license you may not:<br /><br />
+		Permission is granted to download one copy of the materials (information or software)<br /> 
+                on CodeJobs.org's web site for personal. This is the grant of a license,<br />
+		not a transfer of title, and under this license you are free to:<br /><br />
 		
 		<ol type="i">
-			<li>modify or copy the materials;</li>
-			<li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
-			<li>attempt to decompile or reverse engineer any software contained on CodeJobs.org's web site;</li>
-			<li>remove any copyright or other proprietary notations from the materials; or</li>
-			<li>transfer the materials to another person or "mirror" the materials on any other server.</li><br />
+			<li>to copy, distribute and transmit the work ;</li>
+			<li>to adapt the work ;</li>
+			<li>to make commercial use of the work ;</li>
 		</ol>
 	</li>
+         <br />
 	<li>
-		This license shall automatically terminate if you violate any of these restrictions and may be terminated <br />
-                by CodeJobs.org at any time. Upon terminating your viewing of these materials or upon the termination of this <br />
-                license, you must destroy any downloaded materials in your possession whether in electronic or printed format.
+		Under the following conditions: 
+
+                <ol type="i">
+                        <li> You must attribute the work in the manner specified by the author or licensor <br />
+                             (but not in any way that suggests that they endorse you or your use of the work) ;</li>
+                        <li>If you alter, transform, or build upon this work, you may distribute the resulting work <br />
+                            only under the same or similar license to this one ;</li>
+                </ol>
+
 	</li>
 </ol>
 
